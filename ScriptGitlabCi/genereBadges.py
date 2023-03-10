4caf56c27c1b7c612a608aaa095f4e4e62e457e7
Damien Gieules <damien.gieules@grenoble-inp.org>
2023-03-10 22:55:49 +0100

    correction gitlab ci pour les badges

--- ScriptGitlabCi/genereBadges.py
+++ ScriptGitlabCi/genereBadges.py
@@ -1,36 +1,24 @@
 # coding=utf-8
-
 import subprocess
 import sys
 
-NBERR=0 #on compte le nombre d'erreurs
-NBWARN=0 #et de warnings
 color="green" #le badge est par défaut vert
+message = ""
 
 NBDIAGRAMS=subprocess.run("ls documentation/client/ | egrep \"*drawio\" | wc -l", shell=True, check=True, text=True)
 NBDIAGRAMSGENERE=subprocess.run("ls documentation/client/out | wc -l", shell=True, check=True, text=True)
 
-
-
-# NBDIAGRAMS=subprocess.run(['ls ', 'documentation/client/', '|', 'egrep *drawio', '|', 'wc', '-l'], shell=True, check=True, text=True)
-# NBDIAGRAMSGENERE=subprocess.run(['ls ', 'documentation/client/out', '|', 'wc', '-l'], shell=True, check=True, text=True)
-
 #On regarde s'il y a une difference
-if NBDIAGRAMS == NBDIAGRAMSGENERE:
-    NBWARN = 0
-else:
-    NBERR = 1
-
-if NBERR > 0:
+if NBDIAGRAMS != NBDIAGRAMSGENERE:
     color = "red"
-elif NBWARN > 0:
-    color = "orange"
-
+    message = Erreurs diagrammes
+else:
+    message = Diagrammes OK
 
 
 
 #on appelle anybadge pour faire un badge .svg
-subprocess.run(f"""anybadge -o -l \"Drawio\" -v \"{NBERR} erreur {NBWARN} warning\" -c \"{color}\" -f \"drawio.svg\"""", shell=True, check=True)
+subprocess.run(f"""anybadge -o -l \"Drawio\" -v \"{message}\" -c \"{color}\" -f \"drawio.svg\"""", shell=True, check=True)
 
 
  
