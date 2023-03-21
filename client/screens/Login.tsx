6b18ecf1460b14a5bae03e8d01f76c0f53b22d93
Damien Gieules <damien.gieules@grenoble-inp.org>
2023-03-21 15:07:15 +0100

    change to login

--- client/screens/Login.tsx
+++ client/screens/Login.tsx
@@ -1,5 +1,5 @@
 import { StatusBar } from "expo-status-bar";
-import { ImageBackground, Button, StyleSheet, Text, View } from "react-native";
+import { ImageBackground, Button, StyleSheet, Text, View, Linking } from "react-native";
 import { useNavigation, Link } from "@react-navigation/native";
 import Home from "./Home";
 import { StackNavigation } from "../App";
@@ -17,13 +17,13 @@ const styles = StyleSheet.create({
         flex: 1,
         alignItems: "center",
         justifyContent: "center"
-    }, 
+    },
     image: {
         display: "flex",
         alignItems: "center",
         justifyContent: "center",
         flex: 1,
-        width: '100%', 
+        width: '100%',
         height: '100%'
     },
     texte: {
@@ -33,7 +33,7 @@ const styles = StyleSheet.create({
         fontSize: 25
     },
     TextInput: {
-        
+
         marginTop: 10,
         backgroundColor: "#fff"
     }
@@ -42,10 +42,11 @@ const styles = StyleSheet.create({
 export default function Login() {
     const navigation = useNavigation<StackNavigation>();
 
-    const [count, setCount] = useState(0);
+    const [loginOrRegister, setLoginOrRegister] = useState(0);
 
     const userInputRef = useRef<TextInput>(null);
     const passInputRef = useRef<TextInput>(null);
+    const pseudoInputRef = useRef<TextInput>(null);
 
     const verifyUserAndPass = () => {
         //setCount(count + 1);
@@ -56,19 +57,40 @@ export default function Login() {
         // .catch(e => ...);
     };
 
+    const registerUser = () => {
+        navigation.navigate("Home"); // TODO: A mettre dans le then
+        fetch(`${API_BASE_URL}login`)
+            .then(res => res.json());
+        // .then(res => setToken(res.token))
+        // .catch(e => ...);
+    };
+
     return (
         <View style={styles.container}>
-            <ImageBackground source={require('../assets/pxArt_6.png')} resizeMode="cover" style={styles.image}> 
-       
-
+            <ImageBackground source={require('../assets/pxArt_6.png')} resizeMode="cover" style={styles.image}>
                 <Box>
-                    <Text style={styles.texte}>Login</Text>
-                    <TextInput placeholder="Login" aria-label="Login" ref={userInputRef} style={styles.TextInput}/>
-                    <TextInput placeholder="Mot de passe" aria-label="Mot de passe" ref={passInputRef} style={styles.TextInput} secureTextEntry />
-                    <Button onPress={verifyUserAndPass} title="Login" />
-                </Box>
+                    {loginOrRegister === 0 ?
+                        <><Text style={styles.texte}>Login</Text>
+                            <TextInput placeholder="Login" aria-label="Login" ref={userInputRef} style={styles.TextInput} />
+                            <TextInput placeholder="Mot de passe" aria-label="Mot de passe" ref={passInputRef} style={styles.TextInput} secureTextEntry />
+                            <Button onPress={verifyUserAndPass} title="Login" />
+                            <Text style={{ color: 'white', marginTop: 4}}
+                                onPress={() => setLoginOrRegister(loginOrRegister + 1)}>
+                                Vous n'avez pas de compte, Enregistez vous!
+                            </Text></> :
 
-            
+                        <> <Text style={styles.texte}>Register</Text>
+                            <TextInput placeholder="Pseudo" aria-label="Pseudo" ref={pseudoInputRef} style={styles.TextInput} />
+                            <TextInput placeholder="Mot de passe" aria-label="Mot de passe" ref={passInputRef} style={styles.TextInput} secureTextEntry />
+                            <Button onPress={registerUser} title="Register" />
+                            
+                            <Text style={{ color: 'white', marginTop: 4 }}
+                                onPress={() => setLoginOrRegister(loginOrRegister - 1)}>
+                                Vous avez deja un compte, Connectez vous!
+                            </Text></>
+                            
+                    }
+                </Box>
             </ImageBackground>
         </View>
     );
