4593a653d3c5f28e14a08cd2c2564f4804fc7c52
Samuel Thiken <samuel.thiken@grenoble-inp.org>
2023-03-21 13:40:05 +0100

    fonts

--- client/App.tsx
+++ client/App.tsx
@@ -4,6 +4,7 @@ import Login from "./screens/Login";
 import { createStackNavigator } from "@react-navigation/stack";
 import { UserProvider } from "./context/UserContext";
 import { SettingsProvider } from "./context/SettingsContext";
+import { useFonts } from "expo-font";
 
 export type StackParamList = {
     Home: undefined;
@@ -16,12 +17,16 @@ function MyStack() {
     return (
         <Stack.Navigator initialRouteName="Login">
             <Stack.Screen name="Home" component={Home} />
-            <Stack.Screen name="Login" component={Login} options={{headerShown: false}}/>
+            <Stack.Screen name="Login" component={Login} options={{ headerShown: false }} />
         </Stack.Navigator>
     );
 }
 
 export default function App() {
+    useFonts({
+        pixel: require("./assets/fonts/depixel.ttf")
+    });
+
     return (
         <SettingsProvider>
             <UserProvider>
