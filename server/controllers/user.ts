d495fc472cbf6a567a5ce13fefe8bb19455d7b0e
Bruno CARRÈRE <bruno.carrere@grenoble-inp.org>
2023-03-26 17:49:59 +0200

    get token

--- server/controllers/user.ts
+++ server/controllers/user.ts
@@ -1,6 +1,18 @@
 import jwt from "jsonwebtoken";
 import { Request, Response } from "express";
 
+function validToken(token) {
+    const isValid: boolean = jwt.verify(token, "secret", { algorithms: ["RS256"] });
+    if (!isValid) throw new Error("Invalid Token !");
+
+    const username = jwt.decode(token, "secret", { algorithms: ["RS256"] }).username;
+    const password = jwt.decode(token, "secret", { algorithms: ["RS256"] }).password;
+    return {
+        username: username,
+        password: password
+    };
+}
+
 export const getToken = async (req: Request, res: Response) => {
     const username: string = req.body.username;
     const password: string = req.body.password;
@@ -10,11 +22,11 @@ export const getToken = async (req: Request, res: Response) => {
             username: username,
             password: password
         },
-        "secret"
+        "secret",
+        { algorithm: "RS256" }
     );
 
     res.status(200).json({
         token: token
     });
 };
-
