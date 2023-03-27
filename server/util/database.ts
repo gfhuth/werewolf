e3c07d293127a915491f8b5b34996f072bfd10a4
Samuel Thiken <samuel.thiken@grenoble-inp.org>
2023-03-27 12:53:54 +0200

    database

--- server/util/database.ts
+++ server/util/database.ts
@@ -1,10 +1,18 @@
 import betterSqlite3 from "better-sqlite3";
-import fs from "fs";
+import { Kysely, SqliteDialect } from "kysely";
+import { Database } from "./sql/schema";
 
-const connection = betterSqlite3("db.sqlite");
+import fs from "fs";
+const db = new Kysely<Database>({
+    dialect: new SqliteDialect({
+        database: betterSqlite3("db.sqlite")
+    })
+});
 
 export const createSchema = async (): Promise<void> => {
-    connection.exec(fs.readFileSync(`${__dirname}/sql/schema.sql`).toString());
+    // connection.exec(fs.readFileSync(`${__dirname}/sql/schema.sql`).toString());
+    await db.schema.createTable("users").addColumn("id", "integer").addColumn("username", "text").addColumn("password", "text").execute();
+    console.log(await db.insertInto("users").values({ username: "Bruno", password: "1234" }).returning("id").executeTakeFirstOrThrow());
 };
 
-export default connection;
+export default db;
