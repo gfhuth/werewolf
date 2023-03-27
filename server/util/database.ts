52132cf9cb6ee650c51cee4daa1584b92c6e67b7
Bruno CARRÈRE <bruno.carrere@grenoble-inp.org>
2023-03-27 12:31:08 +0200

    pour samuel

--- server/util/database.ts
+++ server/util/database.ts
@@ -1,17 +1,10 @@
-import { Database } from "sqlite3";
+import betterSqlite3 from "better-sqlite3";
 import fs from "fs";
 
-const db = new Database("db.sqlite");
-
-function exec(query: string): Promise<void> {
-    return new Promise((resolve, reject) => {
-        db.exec(query, (err) => {
-            if (err) reject(err);
-            else resolve();
-        });
-    });
-}
+const connection = betterSqlite3("db.sqlite");
 
 export const createSchema = async (): Promise<void> => {
-    await exec(fs.readFileSync(`${__dirname}/sql/schema.sql`).toString());
+    connection.exec(fs.readFileSync(`${__dirname}/sql/schema.sql`).toString());
 };
+
+export default connection;
