d1080dff33dc537c6295ac040b19d4826b872837
Samuel Thiken <samuel.thiken@grenoble-inp.org>
2023-03-28 11:32:51 +0200

    request api

---
+++ client/utils/request.ts
@@ -0,0 +1,30 @@
+export type RequestError = {
+    message: string;
+    status: number;
+    response?: Response;
+    error?: any;
+};
+
+export default function request(input: RequestInfo | URL, init?: RequestInit | undefined): Promise<Response> {
+    return new Promise((resolve, reject) => {
+        fetch(input, init)
+            .then((res) => {
+                if (res.ok) {
+                    resolve(res);
+                } else {
+                    reject({
+                        message: res.body,
+                        status: res.status,
+                        response: res
+                    });
+                }
+            })
+            .catch((e) => {
+                reject({
+                    message: e.message,
+                    status: -1,
+                    error: e
+                });
+            });
+    });
+}
