6a964c35b8042d037a929a85920dfd95f771ea5d
Bruno CARRÈRE <bruno.carrere@grenoble-inp.org>
2023-04-07 14:17:46 +0200

    route for user

---
+++ server/routers/user.ts
@@ -0,0 +1,12 @@
+import express from "express";
+import { verifyToken } from "../controllers/authenticationController";
+import { debugUser, login, register, reinitDatabase, whoAmI } from "../controllers/userController";
+const userRouter = express.Router();
+
+userRouter.post("/login", login);
+userRouter.get("/whoami", whoAmI);
+userRouter.post("/register", register);
+userRouter.post("/reinit", reinitDatabase);
+userRouter.get("/debug", debugUser);
+
+export default userRouter;
--- server/server.ts
+++ server/server.ts
@@ -12,6 +12,7 @@ import { newGame } from "./controllers/gameController";
 import { newChat } from "./controllers/chatController";
 import { listMessages, newMessage } from "./controllers/messageController";
 import { onConnect } from "./controllers/websocketController";
+import userRouter from "./routers/user";
 
 const app = express();
 
@@ -30,28 +31,7 @@ app.use(
 );
 app.use(express.json());
 app.use("/game", gameRouter);
-
-//START move this part in routers/user.ts
-app.post("/user/login", login);
-app.get("/user/whoami", whoAmI);
-app.post("/user/register", register);
-app.post("/user/reinit", reinitDatabase);
-app.get("/user/debug", debugUser);
-// END PART TO MOVE
-
-//START move this part in routers/game.ts
-// app.get("/game/search", searchGame);
-app.post("/game/new", newGame);
-// END PART TO MOVE
-
-//START move this part in routers/chat.ts
-app.post("/game/:gameid/chat/:id", newChat);
-// END PART TO MOVE
-
-//START move this part in routers/message.ts
-app.post("/game/:gameid/chat/:id/message/new", newMessage);
-app.get("/game/:gameid/chat/:chatid/message", listMessages);
-// END PART TO MOVE
+app.use("/user", userRouter);
 
 wss.on("connection", (ws: WebSocket) => {
     onConnect(ws);
