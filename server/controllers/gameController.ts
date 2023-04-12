fdbea3041c2e14f64d145b621eafbab21f83a8a8
Dorian Voland <dorian.voland@gmail.com>
2023-04-12 11:29:17 +0200

    join game

--- server/controllers/gameController.ts
+++ server/controllers/gameController.ts
@@ -1,7 +1,12 @@
 import { Request, Response } from "express";
+import { sql } from "kysely";
 import { getTokenContent } from "./userController";
 import { Game, GameParam, gamesList } from "../models/gameModel";
 import database from "../util/database";
+
+async function getUserId(username): Promise<number> {
+    return (await database.selectFrom("users").select(["id"]).where("username", "=", username).executeTakeFirst()).id;
+}
 
 export async function searchGame(req: Request, res: Response): Promise<void> {
     //game list from SQLdatabase;
@@ -41,11 +46,9 @@ export async function searchGameById(req: Request, res: Response): Promise<void>
 export async function searchGameByUsername(req: Request, res: Response): Promise<void> {
     try {
         const username: string = getTokenContent(req.headers["x-access-token"] as string).username;
-        if (!username) throw new Error("No username provided.");
-        console.log(username);
+        const userId: number = await getUserId(username);
         // Récupérer les jeux depuis la base de données SQL avec le nom d'utilisateur
-        const games = await database.selectFrom("games").selectAll().where("games.hostname", "=", username).execute();
-        console.log(games);
+        const games = await database.selectFrom("games").selectAll().where("games.hostId", "=", userId).execute();
         // Convertir le jeu en JSON et l'envoyer dans la réponse
         const gamesJson = [];
         games.forEach((game) => {
@@ -61,6 +64,7 @@ export async function searchGameByUsername(req: Request, res: Response): Promise
 export const newGame = async (req: Request, res: Response): Promise<void> => {
     console.log("verif en cours");
     const hostName = getTokenContent(req.headers["x-access-token"] as string).username;
+    const hostId = await getUserId(hostName);
     // Valeur par défaut de la date
     const defaultDate = new Date();
     defaultDate.setDate(defaultDate.getDate() + 1);
@@ -71,20 +75,21 @@ export const newGame = async (req: Request, res: Response): Promise<void> => {
     const today = new Date();
 
     const game = {
-        hostName: hostName,
+        hostId: hostId,
         currentNumberOfPlayer: 1,
         nbPlayerMin: req.body.nbPlayerMin || 5,
         nbPlayerMax: req.body.nbPlayerMax || 20,
         dayLength: req.body.dayLength || 60 * 14,
         nightLength: req.body.nightLength || 60 * 10,
         startDate: new Date(req.body.startDate).getTime() || defaultDate.getTime(),
+
         percentageWerewolf: req.body.percentageWerewolf || 0.33,
         probaContamination: req.body.probaContamination || 0,
         probaInsomnie: req.body.probaInsomnie || 0,
         probaVoyance: req.body.probaVoyance || 0,
         probaSpiritisme: req.body.probaSpiritisme || 0
     };
-
+    console.log(defaultDate.getTime());
     const conditions = [
         { minRange: 0, value: game.probaContamination, maxRange: 1, errorMessage: "Unvalid contamination probability" },
         { minRange: 0, value: game.probaInsomnie, maxRange: 1, errorMessage: "Unvalid contamination probability" },
@@ -109,15 +114,58 @@ export const newGame = async (req: Request, res: Response): Promise<void> => {
     try {
         console.log("creation en cours");
         await database.insertInto("games").values(game).execute();
+        res.status(200).json({ message: "New game created" });
     } catch (e) {
         res.sendStatus(500);
     }
+};
 
-    res.status(200).json({ message: "New game created" });
+export const joinGame = async (req: Request, res: Response): Promise<void> => {
+    try {
+        const username: string = getTokenContent(req.headers["x-access-token"] as string).username;
+        const gameId: number = parseInt(req.params.id);
+        if (!gameId) throw new Error("No game ID provided.");
+
+        // Fetch the user ID using the username
+        const userId: number = await getUserId(username);
+
+        //check if player already in game
+        const existingPlayer = await database.selectFrom("players").select(["id"]).where("players.user", "=", userId).where("players.game", "=", gameId).executeTakeFirst();
+        if (existingPlayer) throw new Error("User is already in the game.");
+
+        const game = await database.selectFrom("games").selectAll().where("id", "=", gameId).limit(1).executeTakeFirst();
+        if (!game) throw new Error("Game ID not found");
+        if (game.currentNumberOfPlayer >= game.nbPlayerMax) throw new Error("Game full");
+
+        // Insert a new record in the user_games table
+        await database
+            .insertInto("players")
+            .values({
+                name: username,
+                role: "",
+                power: "",
+                user: userId,
+                game: gameId
+            })
+            .execute();
+        await database
+            .updateTable("games")
+            .set({ currentNumberOfPlayer: game.currentNumberOfPlayer + 1 })
+            .where("id", "=", gameId)
+            .executeTakeFirst();
+
+        res.status(200).json({ message: "game successfully join" });
+    } catch (err) {
+        console.log(err);
+        res.status(500).json({ message: err.message });
+    }
+};
+
+export const leaveGame = async (req: Request, res: Response): Promise<void> => {
+    res.status(404).json({ message: "Not implemented yet" });
 };
 
 export const getGame = (gameId: number): Game => {
-    for (const game of gamesList) 
-        if (game.getGameId() === gameId) return game;
+    for (const game of gamesList) if (game.getGameId() === gameId) return game;
     return null;
 };
