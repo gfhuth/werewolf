12ec6670513b99f4a55959054c2d5b0f148b7664
Dorian Voland <dorian.voland@gmail.com>
2023-04-13 21:26:35 +0200

    add comment

--- server/controllers/gameStartedController.ts
+++ server/controllers/gameStartedController.ts
@@ -1,22 +1,39 @@
 import { Game } from "../models/gameModel";
 import database from "../util/database";
 
+/** Add all the player who have join the game in the player list of game
+ * @param {Game} game game to add player
+ */
 function addPlayerInGame(game: Game): void {
     console.log("TODO : Adding player in game");
 }
+
+/** Set all role in the game
+ * @param {Game} game Game with all player added
+ */
 function setupGame(game: Game): void {
     console.log("TODO : Start game by setting all role and data");
 }
 
+/** Apply all action happend during the night and lunch a day
+ * @param {Game} game Game to apply change
+ */
 function startDay(game: Game): void {
     console.log("TODO : The sun is rising");
 }
+
+/** Apply all action happend during the day and lunch a night
+ * @param {Game} game Game to apply change
+ */
 function startNight(game: Game): void {
     console.log("TODO : The night is arriving");
 }
-/// Function call when a game start
-/// Setup the game (probability, role, ...)
-/// Add event call at each end of days (use interval or timeout), ...
+
+/** Function to add when a game is restored or start
+ * Setup the game (probability, role, ...)
+ * Add event call at each end of days (use interval or timeout), ...
+ * @param {number} gameID id of the starting game 
+ * */ 
 export function initGame(gameID: number): void {
     const gameDb = database.selectFrom("games").selectAll().where("id", "=", gameID).executeTakeFirst();
     const game = Game.gameDBtoGame(gameDb);
@@ -25,7 +42,7 @@ export function initGame(gameID: number): void {
     if (gameStatus <= 1) setupGame(game);
 
     console.log(`game ${gameID} successfuly started`);
-    if (gameStatus % 2 == 0) 
+    if (gameStatus % 2 == 1) 
         startDay(game);
     else 
         startNight(game);
--- server/models/gameModel.ts
+++ server/models/gameModel.ts
@@ -10,6 +10,7 @@ export enum GameStatus {
     JOUR = 1,
     NUIT = 2,
 }
+
 export type GameParam = {
     nbPlayerMin: number;
     nbPlayerMax: number;
@@ -21,30 +22,56 @@ export type GameParam = {
     probaInsomnie: number;
     probaVoyance: number;
     probaSpiritisme: number;
-};
+};  
 
+/** class Game
+ * represente a game
+ * usefield
+ *  currentNumberOfPlayer current number of player who have join the game
+ * fonction
+ *  getGameId()         return the id of this game
+ *  getGameParam()      return the GameParam object of this game
+ *  toShortJson()       return a json with the minimum information
+ *  toLongJson()        return json of this object
+ *  gameDBtoGame(game)  convert a game given by the database in Game object
+ *  getStatus()         Return an number corresponding to the status of the game
+ */
 export class Game {
 
     private gameId: number;
     private gameParam: GameParam;
-    private playerList:Player[] = [];
+    private playerList: Player[] = [];
     private host: User;
     public currentNumberOfPlayer = 1;
 
+    /**
+     * @param {number} gameId is the id of the game
+     * @param {GameParam} gameParam parameter of the game
+     * @param {User} host user who create the game
+     */
     constructor(gameId: number, gameParam: GameParam, host: User) {
         this.gameId = gameId;
         this.gameParam = gameParam;
         this.host = host;
     }
 
-    getGameId(): number {
+    /** Return Id of the game
+     * @returns {number} Id of the game
+     */
+    public getGameId(): number {
         return this.gameId;
     }
 
+    /** Give the GameParam object of this game
+     * @returns {number} parameter of the game
+     */
     public getGameParam(): GameParam {
         return this.gameParam;
     }
-    //return json with : id, startDate, hostName, currentNumberOfPlayer, nbPlayerMax
+
+    /** Give a short json to semarize a games
+     * @returns {JSON} json return
+     */
     public toShortJson(): { [key: string]: string | number } {
         const id = this.gameId;
         const startDate = this.gameParam.startDate;
@@ -60,7 +87,9 @@ export class Game {
         };
     }
 
-    //return json of this object
+    /** Give a json of all information on a game
+     * @returns {JSON} all information of the game
+     */
     public toLongJson(): { [key: string]: string | number } {
         const gameParam = this.gameParam;
         const host = this.host;
@@ -84,37 +113,49 @@ export class Game {
         };
     }
 
-    public static gameDBtoGame(row): Game {
+    /**
+     * Convert a game from the database in object Game
+     * @param {Object} data object given by a request as "db.selectFrom("games").selectAll()"
+     * @returns {Game} a new game create from the given data
+     */
+    public static gameDBtoGame(data): Game {
         const gameParam: GameParam = {
-            nbPlayerMin: row.nbPlayerMin,
-            nbPlayerMax: row.nbPlayerMax,
-            dayLength: row.dayLength,
-            nightLength: row.nightLength,
-            startDate: row.startDate,
-            percentageWerewolf: row.percentageWerewolf,
-            probaContamination: row.probaContamination,
-            probaInsomnie: row.probaInsomnie,
-            probaVoyance: row.probaVoyance,
-            probaSpiritisme: row.probaSpiritisme
+            nbPlayerMin: data.nbPlayerMin,
+            nbPlayerMax: data.nbPlayerMax,
+            dayLength: data.dayLength,
+            nightLength: data.nightLength,
+            startDate: data.startDate,
+            percentageWerewolf: data.percentageWerewolf,
+            probaContamination: data.probaContamination,
+            probaInsomnie: data.probaInsomnie,
+            probaVoyance: data.probaVoyance,
+            probaSpiritisme: data.probaSpiritisme
         };
 
-        const game = new Game(Number(row.id), gameParam, row.hostId);
-        game.currentNumberOfPlayer = row.currentNumberOfPlayer;
+        const game = new Game(Number(data.id), gameParam, data.hostId);
+        game.currentNumberOfPlayer = data.currentNumberOfPlayer;
         return game;
     }
 
-    /// RETURN NUMBER of day/night cyclce passed
+    /** Compute the status of the game
+     * Odd number corresponding to a night. Other are days.
+     * 0 = not started, 1 = first day, 2 = first night
+     * @returns {number} 
+     */
     public getStatus(): number {
         // time in ms
         const time = Date.now() - this.gameParam.startDate;
         const timeInHour = time / 3600000;
         if (time < 0) return GameStatus.NOT_STARTED;
-        else return (Math.floor(timeInHour / (this.gameParam.dayLength + this.gameParam.nightLength)));
+        else return Math.floor(timeInHour / (this.gameParam.dayLength + this.gameParam.nightLength));
     }
 
 }
-export const gameInProgress = [];
 
+/**
+ * Function use to create a game table in the database if it is necessary
+ * Next, load all game in the database and create an event to start a game at the starting date
+ */
 export const gameSchema = async (): Promise<void> => {
     await database.schema
         .createTable("games")
