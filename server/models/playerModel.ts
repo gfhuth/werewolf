fcd3eda6ee5c11b3caa54e21b5357c4415fca8ad
Bruno CARRÈRE <bruno.carrere@grenoble-inp.org>
2023-04-14 12:39:33 +0200

    allow role to be null

--- server/models/playerModel.ts
+++ server/models/playerModel.ts
@@ -24,7 +24,7 @@ export const playerSchema = async (): Promise<void> => {
         .ifNotExists()
         .addColumn("id", "integer", (col) => col.autoIncrement().primaryKey())
         .addColumn("name", "text", (col) => col.notNull())
-        .addColumn("role", "text", (col) => col.notNull())
+        .addColumn("role", "text")
         .addColumn("power", "text")
         .addColumn("user", "integer", (col) => col.references("users.id").onDelete("cascade"))
         .addColumn("game", "integer", (col) => col.references("games.id").onDelete("cascade"))
