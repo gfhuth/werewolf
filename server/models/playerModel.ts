56c91130213c88b187e09221dfbd6e3a3ddf640b
Bruno CARRÈRE <bruno.carrere@grenoble-inp.org>
2023-04-16 17:46:18 +0200

    envoie de messages par un joueur

--- server/models/playerModel.ts
+++ server/models/playerModel.ts
@@ -1,19 +1,38 @@
 import database from "../util/database";
+import { Chat, Message } from "./chatModel";
 import { Game } from "./gameModel";
+import { User } from "./userModel";
 
 export class Player {
 
-    public id: number;
-    public name: number;
-    public role: number;
-    public power: number;
-    public game: Game;
-    constructor(id: number, name: number, role: number, power: number, game: Game) {
-        this.id = id;
-        this.name = name;
+    private user: User;
+    private role: number;
+    private power: number;
+    private game: Game;
+
+    constructor(user: User, role: number, power: number, game: Game) {
+        this.user = user;
         this.role = role;
         this.power = power;
         this.game = game;
+    }
+
+    public getUser(): User {
+        return this.user;
+    }
+
+    public getRole(): number {
+        return this.role;
+    }
+
+    public addMessage(message: Message): void {
+        // On récupère le chat concerné
+        const chat: Chat = this.game
+            .getChats()
+            .reverse()
+            .find((c) => c.getType() === message.type);
+        // On envoie le message sur ce chat
+        chat.addMessage(message, this.user);
     }
 
 }
@@ -24,8 +43,8 @@ export const playerSchema = async (): Promise<void> => {
         .ifNotExists()
         .addColumn("id", "integer", (col) => col.autoIncrement().primaryKey())
         .addColumn("name", "text", (col) => col.notNull())
-        .addColumn("role", "text")
-        .addColumn("power", "text")
+        .addColumn("role", "integer")
+        .addColumn("power", "integer")
         .addColumn("user", "integer", (col) => col.references("users.id").onDelete("cascade"))
         .addColumn("game", "integer", (col) => col.references("games.id").onDelete("cascade"))
         .execute();
