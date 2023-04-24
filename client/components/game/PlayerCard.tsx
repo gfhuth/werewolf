9a050498ce7a96d97e1cbabc85e5bb15728278bd
Samuel Thiken <samuel.thiken@grenoble-inp.org>
2023-04-24 14:40:24 +0200

    Updated player cards

--- client/components/game/PlayerCard.tsx
+++ client/components/game/PlayerCard.tsx
@@ -19,19 +19,21 @@ export default function PlayerCard(props: { player: Player }): React.ReactElemen
     };
 
     return (
-        <Box bg="light.100" p={2}>
+        <Box bg="light.100" borderRadius={5} p={2}>
             <Center>
-                <Pressable onPress={(): any => setIsOpen(!isOpen)}>
-                    <Container display={"flex"} flexDirection={"row"} justifyContent={"space-between"} alignItems={"center"} mb={2} style={{ gap: 3 }}>
-                        {props.player.roles.map((role, i) => (
-                            <Tooltip key={i} label={role.name} placement="top">
-                                <Image alt={`${role.name} role image`} source={images[role.name].uri} width={30} height={30} resizeMode="cover" />
-                            </Tooltip>
-                        ))}
-                    </Container>
+                <Pressable onPress={(): any => setIsOpen(!isOpen)} display={"flex"} flexDirection={"row"}>
                     <Image alt="Player image" source={require("../../assets/images/player.png")} width={70} height={70} resizeMode="cover" />
-                    <Text>{props.player.username}</Text>
-                    {isOpen ? <Button onPress={vote}> Voter </Button> : <Hidden children={null}></Hidden>}
+                    <Container>
+                        <Container display={"flex"} flexDirection={"row"} justifyContent={"space-between"} alignItems={"center"} mb={2} style={{ gap: 3 }}>
+                            {props.player.roles.map((role, i) => (
+                                <Tooltip key={i} label={role.name} placement="top">
+                                    <Image alt={`${role.name} role image`} source={images[role.name].uri} width={30} height={30} resizeMode="cover" />
+                                </Tooltip>
+                            ))}
+                        </Container>
+                        <Text>{props.player.username}</Text>
+                    </Container>
+                    {/* {isOpen ? <Button onPress={vote}> Voter </Button> : <Hidden children={null}></Hidden>} */}
                 </Pressable>
             </Center>
         </Box>
