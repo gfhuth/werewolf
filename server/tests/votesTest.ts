978c6e1113ab3d5baa1e34225c9b0eb8d881b06f
Bruno CARRÈRE <bruno.carrere@grenoble-inp.org>
2023-04-24 13:00:55 +0200

    Ajout du test des votes

--- server/tests/votesTest.ts
+++ server/tests/votesTest.ts
@@ -1,4 +1,4 @@
-import { Chat_type } from "../models/chatModel";
+import { Vote_type } from "../models/voteModel";
 import { Client } from "./websocketsTest";
 import { token } from "./usersTest";
 
@@ -13,28 +13,24 @@ afterAll(() => {
     client.closeSocket();
 });
 
-describe("Test chats", () => {
+describe("Test votes", () => {
     test("Authenticate client on websocket", async () => {
         const isAuthenticated: boolean = await client.authenticate(token);
         expect(isAuthenticated).toEqual(true);
     });
     test("Send message", async () => {
-        const now: number = Date.now();
         client.sendMessage(
             JSON.stringify({
                 game_id: 1,
-                event: "CHAT_SENT",
+                event: "VOTE_SENT",
                 data: {
-                    date: now,
-                    chat_type: Chat_type.CHAT_VILLAGE,
-                    content: "Premier message"
+                    vote_type: Vote_type.VOTE_VILLAGE,
+                    vote: "carrereb"
                 }
             })
         );
-        const res: Record<string, any> = await client.getNextEvent("CHAT_RECEIVED");
-        expect(res.event).toEqual("CHAT_RECEIVED");
-        expect(res.data.chat_type).toEqual(Chat_type.CHAT_VILLAGE);
-        expect(res.data.date).toEqual(now);
-        expect(res.data.content).toEqual("Premier message");
+        const res: Record<string, any> = await client.getNextEvent("VOTE_RECEIVED");
+        expect(res.event).toEqual("VOTE_RECEIVED");
+        expect(res.data.vote_type).toEqual(Vote_type.VOTE_VILLAGE);
     });
 });
