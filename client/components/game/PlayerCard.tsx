06f31692f8de5b1e85ba8b5a2ff718c8f59958ad
Damien Gieules <damien.gieules@grenoble-inp.org>
2023-04-24 14:51:16 +0200

    action sheet

--- client/components/game/PlayerCard.tsx
+++ client/components/game/PlayerCard.tsx
@@ -1,4 +1,4 @@
-import { Box, Button, Center, Container, Hidden, Image, Pressable, Text, Tooltip } from "native-base";
+import { Actionsheet, Box, Button, Center, Container, Hidden, Image, Pressable, Text, Tooltip, useDisclose } from "native-base";
 import { images } from "./image";
 import { useState } from "react";
 
@@ -12,16 +12,16 @@ export type Player = {
 };
 
 export default function PlayerCard(props: { player: Player }): React.ReactElement {
-    const [isOpen, setIsOpen] = useState(false);
+    const [isOpenVote, setIsOpen] = useState(false);
 
     const vote = (): void => {
         console.log("A VOTE");
     };
-
+    const { isOpen, onOpen, onClose } = useDisclose();
     return (
         <Box bg="light.100" p={2}>
             <Center>
-                <Pressable onPress={(): any => setIsOpen(!isOpen)}>
+                <Pressable onPress={onOpen}>
                     <Container display={"flex"} flexDirection={"row"} justifyContent={"space-between"} alignItems={"center"} mb={2} style={{ gap: 3 }}>
                         {props.player.roles.map((role, i) => (
                             <Tooltip key={i} label={role.name} placement="top">
@@ -31,8 +31,29 @@ export default function PlayerCard(props: { player: Player }): React.ReactElemen
                     </Container>
                     <Image alt="Player image" source={require("../../assets/images/player.png")} width={70} height={70} resizeMode="cover" />
                     <Text>{props.player.username}</Text>
-                    {isOpen ? <Button onPress={vote}> Voter </Button> : <Hidden children={null}></Hidden>}
                 </Pressable>
+
+                <Actionsheet isOpen={isOpen} onClose={onClose}>
+                    <Actionsheet.Content>
+                        <Box w="100%" h={60} px={4} justifyContent="center">
+                            <Text
+                                fontSize="16"
+                                color="gray.500"
+                                bold
+                                _dark={{
+                                    color: "gray.300"
+                                }}
+                            >
+                                Actions possibles :
+                            </Text>
+                        </Box>
+                        <Actionsheet.Item>VOTE</Actionsheet.Item>
+                        <Actionsheet.Item isDisabled>Share</Actionsheet.Item>
+                        <Actionsheet.Item>Play</Actionsheet.Item>
+                        <Actionsheet.Item>Favourite</Actionsheet.Item>
+                        <Actionsheet.Item>Cancel</Actionsheet.Item>
+                    </Actionsheet.Content>
+                </Actionsheet>
             </Center>
         </Box>
     );
