34982d831d8d6815ab0853c6ed1220da05757ab5
Bruno CARRÈRE <bruno.carrere@grenoble-inp.org>
2023-05-09 20:43:43 +0200

    api de test

--- server/tests/test-api/testAPI.ts
+++ server/tests/test-api/testAPI.ts
@@ -30,18 +30,54 @@ export class Assert {
         console.log(colors.grey(`ERROR assertion: assert - ${object} is not true`));
     }
 
+    // public async testOrTimeout(func: Promise<boolean>, timeout = 5000): Promise<void> {
+    //     try {
+    //         await new Promise<boolean>(async (resolve, reject) => {
+    //             const id: NodeJS.Timeout = setTimeout(() => {
+    //                 reject(false);
+    //             }, timeout);
+    //             // Test
+    //             func.then((retval) => {
+    //                 resolve(retval);
+    //                 if (!retval) {
+    //                     this.nbErrors += 1;
+    //                     console.log(colors.grey(`ERROR assertion: assert - return value is false`));
+    //                 }
+    //                 clearTimeout(id);
+    //             }).catch(reject);
+    //         });
+    //     } catch (e) {
+    //         this.nbErrors += 1;
+    //         console.log(colors.grey(`ERROR timeout - ${timeout} ms`));
+    //     }
+    // }
+
+    public async testOrTimeout(func: Promise<boolean>, timeout = 5000): Promise<void> {
+        try {
+            const result = await Promise.race([new Promise((resolve, reject) => setTimeout(reject, timeout)), func]);
+            if (!result) {
+                this.nbErrors += 1;
+                console.log(colors.grey(`ERROR assertion: assert - return value is false`));
+            }
+        } catch (e) {
+            this.nbErrors += 1;
+            console.log(colors.grey(`ERROR timeout - ${timeout} ms`));
+        }
+    }
+
 }
 
 export const test = async (message: string, func: (assert: Assert) => Promise<void>): Promise<void> => {
     nbTests += 1;
     console.log();
     console.log(colors.yellow(`#### ${message} ####`));
+
     const assert: Assert = new Assert();
     await func(assert);
     if (assert.getNbErrors() > 0) {
-        console.log(colors.red("     failed")); 
+        console.log(colors.red("Failed"));
     } else {
-        console.log(colors.green("     succeed"));
+        console.log(colors.green("Succeed"));
         nbSuccess += 1;
     }
 };
