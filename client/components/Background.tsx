2007f15f3be31b507ffc6e40ccd81a4112977886
Samuel Thiken <samuel.thiken@grenoble-inp.org>
2023-05-12 10:33:33 +0200

    background

--- client/components/Background.tsx
+++ client/components/Background.tsx
@@ -6,24 +6,21 @@ import React, { useEffect, useContext, useState } from "react";
 export default function Background(props: { children: React.ReactNode }): React.ReactElement {
     const gameContext = useContext(GameContext);
 
-    const [tempsSeconde, settempsSeconde] = useState<number>(0);
-    const tempsNuit = (gameContext.NightDuration) * 60;
-    const tempsJour = (gameContext.DayDuration) * 60;
+    const tempsNuit = gameContext.NightDuration * 60;
+    const tempsJour = gameContext.DayDuration * 60;
 
-    setInterval(() => {
-        settempsSeconde(tempsSeconde + 1);
-    }, 1000);
+    const [distanceTop, setDistanceTop] = useState(0);
 
-
-    const divStyle = {
-        translateX: tempsSeconde
-    };
-
+    useEffect(() => {
+        setInterval(() => {
+            setDistanceTop((top) => top + 10);
+        }, 1000);
+    }, []);
 
     return (
         <>
-            <Container minHeight={"100%"} minWidth={"100%"} position={"relative"} display={"flex"} flexDirection={"column"} alignItems={"center"} justifyContent={"center"}>
-                <Image source={require("../assets/background2.png")} alt="Background image" position={"absolute"} width={"100%"} height={"100%"} style={divStyle} resizeMode="cover" />
+            <Container minHeight={"100%"} minWidth={"100%"} position={"relative"} display={"flex"} flexDirection={"column"} alignItems={"center"} justifyContent={"center"} overflow={"hidden"}>
+                <Image source={require("../assets/background2.png")} alt="Background image" position={"absolute"} width={"100%"} height={"300%"} top={distanceTop} resizeMode="cover" />
                 <Container pt={50} />
                 {props.children}
             </Container>
